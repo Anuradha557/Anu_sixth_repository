092e60163e8af5fd945c0bbf39a7fed3d04d4675
Anuradha557 <anuradhasurjuse207@gmail.com>
2025-09-17 18:26:37 +0530

    This is my sixth commit

---
+++ main.py
@@ -0,0 +1,10 @@
+lis=[1,2,3,4,5,6,7,8,9,10]
+print("Print all even numbers")
+for i in lis:
+    if i % 2==0:
+        print(i)
+
+print("Print all odd numbers")
+for i in lis:
+    if i % 2!=0:
+        print(i)
